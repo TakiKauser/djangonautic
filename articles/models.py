0af0b1b08f46d9dceb4cd434f8eb75444739d0dc
TakiKauser <takikauserbadhon@gmail.com>
2022-08-15 20:30:54 +0600

    admin

--- articles/models.py
+++ articles/models.py
@@ -11,5 +11,11 @@ class Article(models.Model):
     # add an thumbnail later
     # add an author later
 
+    def __str__(self):
+        return self.title
+
+    def snippet(self):
+        return self.body[:100] + '...'
+
     # python manage.py makemigrations
     # python manage.py migrate
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 def article_list(request):
-    return render(request, "articles/article.html")
+    articles = Article.objects.all().order_by('date')
+
+    return render(request, "articles/article.html", {'articles': articles})
